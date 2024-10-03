d2c0bd4ccb6c641c96a02b21b145bcfe70bf60ea
DerickNorlan <148537104+Dericknorlan@users.noreply.github.com>
2024-10-03 14:54:32 +0800

    Update function_arrays.php

--- Lab_exercise_2_Derick Norlan/function_arrays.php
+++ Lab_exercise_2_Derick Norlan/function_arrays.php
@@ -1,32 +1,22 @@
 <?php
-class functions_arrays {
     function greet($student) {
-        echo 'Hello, ' . $student->name .'<br>';
+        echo 'Hello, ' . $student->name . '<br>';
     }
-}
 
 include "control_flow.php";
-$students = [];
 
-$student2 = new Student("kemal", "Male", "03-04-2005", 3.80);
-$student2->displayInfo();
-echo '<br>';
+// Inisialisasi array dengan objek siswa secara langsung
+$students = [
+    new Student("kemal", "Male", "03-04-2005", 3.80),
+    new Student("aaron", "Male", "06-09-2005", 3.81),
+    new Student("deny", "Male", "25-08-2005", 3.90),
+    new Student("aryo", "Male", "05-06-2004", 3.80)
+];
 
-$student3 = new Student("aaron", "Male", "06-09-2005", 3.81);
-$student3->displayInfo();
-echo '<br>';
-
-$student4 = new Student("deny", "Male", "25-08-2005", 3.90);
-$student4->displayInfo();
-echo '<br>';
-
-$student5 = new Student("aryo", "Male", "05-06-2004", 3.80);
-$student5->displayInfo();
-echo '<br>';
-
-array_push($students, $student, $student2, $student3, $student4, $student5);
-$functions = new functions_arrays();
+// Menampilkan informasi untuk setiap siswa
 foreach ($students as $student) {
-    $functions->greet($student);
+    $student->displayInfo();
+    greet($student);
+    echo '<br>';
 }
 ?>

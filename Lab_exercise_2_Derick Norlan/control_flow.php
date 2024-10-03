4ba0f899f6ef28225370af9560bcf915a9f51355
DerickNorlan <148537104+Dericknorlan@users.noreply.github.com>
2024-10-03 14:54:54 +0800

    Update control_flow.php

--- Lab_exercise_2_Derick Norlan/control_flow.php
+++ Lab_exercise_2_Derick Norlan/control_flow.php
@@ -1,19 +1,19 @@
 <?php
-class ControlFlow {
-    public $definition;
-
-    // Constructor to determine adult or minor
-    public function __construct($age) {
-        $this->definition = ($age >= 18) ? "You are an adult" . '<br><br>' : "You are a minor" . '<br><br>';
+// Fungsi untuk menentukan apakah seseorang dewasa atau minor
+function checkAge($age) {
+    if ($age >= 18) {
+        return "You are an adult<br><br>";
+    } else {
+        return "You are a minor<br><br>";
     }
 }
 
 include "variables.php";
 
-// Check if age is set and create an instance of ControlFlow
+// Memeriksa apakah usia sudah diatur
 if (isset($student->age)) {
-    $controlFlow = new ControlFlow($student->age);
-    echo $controlFlow->definition;
+    // Panggil fungsi checkAge dan tampilkan hasilnya
+    echo checkAge($student->age);
 } else {
     echo "Something went wrong. Check your code!";
 }
